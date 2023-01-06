bb14e0b97a2d375990e6745df4184c32a0872339
Fejise <oghenefejirosodje@gmail.com>
2023-01-06 14:41:56 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,6 @@
 /**
  * main - prints the name of the program
  * @argc: number of arguments
-#include <stdio.h>
-#include "main.h"
-
-/**
- * main - prints the name of the program
- * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
